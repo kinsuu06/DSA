0ce22b39c413ee83453dd9007e27df71340969d8
Krati Agrawal <122533357+kinsuu06@users.noreply.github.com>
2025-04-06 05:53:19 -0700

    Preorder_Traversal

--- Binary Tree/Preorder_Traversal.java
+++ Binary Tree/Preorder_Traversal.java
@@ -11,3 +11,26 @@ class Solution {
         preorder(root.right,ll);
     }
 }
+
+
+// BY ITERATIVE USING STACK
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if(root == null) return ans;
+        st.push(root);
+        while(!st.isEmpty()){
+            root = st.pop();
+            ans.add(root.val);
+            if(root.right != null){
+                st.push(root.right);
+            }
+            if(root.left != null){
+                st.push(root.left);
+            }
+        }
+        return ans;
+    }
+}
