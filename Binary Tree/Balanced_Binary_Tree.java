d4c29d7f8014c15068ce90352ffaa84385ea1212
Krati Agrawal <122533357+kinsuu06@users.noreply.github.com>
2025-04-08 23:19:50 -0700

    Balanced_Binary_Tree

---
+++ Binary Tree/Balanced_Binary_Tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return func(root).isbal;
+    }
+    public bal func(TreeNode root){
+        if(root == null){
+            return new bal();
+        }
+        bal left = func(root.left);
+        bal right = func(root.right);
+        boolean con = Math.abs(left.height-right.height) <= 1;
+        bal self = new bal();
+        self.isbal = con && left.isbal && right.isbal;
+        self.height = Math.max(left.height,right.height)+1;
+        return self;
+    }
+    class bal{
+        boolean isbal = true;
+        int height = -1;
+    }
+}
