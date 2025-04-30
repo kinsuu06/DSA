5e4e9e3b1d51c7036f7377b9c51696c2283203ec
Krati Agrawal <122533357+kinsuu06@users.noreply.github.com>
2025-04-30 16:20:16 +0530

    Create Find numbers with even number of digits.java

---
+++ POTD Leetcode/Find numbers with even number of digits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int ans = 0;
+        for(int i=0; i<nums.length; i++){
+            if(digits(nums[i]) % 2 == 0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+    public int digits(int n){
+        int count = 0;
+        while(n>0){
+            int rem = n%10;
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+}
