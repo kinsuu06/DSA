5e9db8fc525c72c2b21a1cd1a812ec5ed895cbdb
Krati Agrawal <122533357+kinsuu06@users.noreply.github.com>
2025-05-14 16:31:25 +0530

    Create Rotate Array.java

---
+++ Array/Rotate Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k%nums.length;
+        func(nums,0, nums.length-1);
+        func(nums,0,k-1);
+        func(nums, k, nums.length-1);
+    }
+    public void func(int[] nums,int i, int j){
+        
+        while(i < j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+}
