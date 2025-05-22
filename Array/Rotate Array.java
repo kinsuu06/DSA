38d99bde8f2df05483f35e7287e44a2389d8eab5
Krati Agrawal <122533357+kinsuu06@users.noreply.github.com>
2025-05-22 20:40:05 +0530

    Update Rotate Array.java

--- Array/Rotate Array.java
+++ Array/Rotate Array.java
@@ -6,8 +6,7 @@ class Solution {
         func(nums, k, nums.length-1);
     }
     public void func(int[] nums,int i, int j){
-        
-        while(i < j){
+         while(i < j){
             int temp = nums[i];
             nums[i] = nums[j];
             nums[j] = temp;
