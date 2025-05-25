605ca06d731d8184e212648697c7a286063a8f17
Krati Agrawal <122533357+kinsuu06@users.noreply.github.com>
2025-05-25 18:21:33 +0530

    Create Traversal-in-LL.java

---
+++ LinkeList/Traversal-in-LL.java
@@ -0,0 +1,38 @@
+
+class Node{
+  int data;
+  Node next;
+
+  Node(int data, Node next1){
+    this.data = data;
+    this.next = next1;
+  }
+  
+  Node(int data){
+    this.data = data;
+    this.next = null;
+  }
+}
+
+public class Main{
+  private static convertArr2LL(int[] arr){
+    Node head = new Node(arr[0]);
+    Node mover = head;
+    for(int i=1; i<arr.length; i++){
+      Node temp = new Node(arr[i]);
+      mover.next = temp;
+      mover = temp;
+    }
+    return head;
+  }
+  
+  public static void main(String[] args){
+    int[] arr = {2, 4, 6, 7};
+    Node head = convertArr2LL(arr);
+    Node temp = head;
+    while(temp != null){
+      System.out.println(temp.data + " ");
+      temp = temp.next;
+    }
+  }
+}
