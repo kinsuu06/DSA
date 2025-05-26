94c43402814470040b3360bd1310629463d36ebc
Krati Agrawal <122533357+kinsuu06@users.noreply.github.com>
2025-05-26 15:22:17 +0530

    Create Starting-node-of-the-cycle-in-LL.java

---
+++ LinkeList/Starting-node-of-the-cycle-in-LL.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode temp = head;
+        HashMap<ListNode, Integer> map = new HashMap<>();
+        while(temp != null){
+            if(map.containsKey(temp)){
+                return temp;
+            }else{
+                map.put(temp,1);
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+  // optimised
+
+  public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
