8bc7c3419829391232eccb84b2942dab8f76c97c
Krati Agrawal <122533357+kinsuu06@users.noreply.github.com>
2025-06-17 18:32:27 +0530

    Create House Robber.java

---
+++ DP/House Robber.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length+1];
+        return dpSol(nums, 0, dp);
+    }
+    public int dpSol(int[] nums, int i, int[] dp){
+        dp[0] = dp[1] = 0;
+        if(i >= nums.length) return 0;
+        if(dp[i] != 0) return dp[i];
+        int rob = nums[i] + dpSol(nums, i+2, dp);
+        int donotrob = dpSol(nums, i+1, dp);
+        return dp[i] = Math.max(rob,donotrob);
+    }
+}
+
+
+class Solution {
+    public int rob(int[] nums) {
+        return recursion(nums,0);
+    }
+    public int recursion(int[] nums, int i){
+        if(i >= nums.length) return 0;
+        int rob = nums[i] + recursion(nums, i+2);
+        int donotrob = recursion(nums, i+1);
+        return Math.max(rob,donotrob);
+    }
+}
