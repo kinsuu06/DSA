3504a21a17976bb1e01901d37e57895335cac1b4
Krati Agrawal <122533357+kinsuu06@users.noreply.github.com>
2025-06-17 17:13:47 +0530

    Create Climbing Stairs.java

--- DP/Climbing Stairs.java
+++ DP/Climbing Stairs.java
@@ -1,14 +1,31 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n == 0 || n == 1) return 1;
+        return recursion(n);
 
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    public int recursion(int n){
+        if(n < 0){
+            return 0;
         }
-        return dp[n];
+        if(n == 0) return 1;
+        int oneStep = recursion(n-1);
+        int twoStep = recursion(n-2);
+        return oneStep+twoStep;
+    }
+}
+
+// DP
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        return func(n,dp);
+    }
+    public int func(int n, int[] dp){
+        dp[0] = dp[1] = 1;
+        if(dp[n] != 0) return dp[n];
+        int oneStep = func(n-1,dp);
+        int twoStep = func(n-2,dp);
+        return dp[n] = oneStep+twoStep;
     }
 }
